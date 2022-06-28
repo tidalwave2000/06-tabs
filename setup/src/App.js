aab9317c668051c3ddf6b746dca99843dd61a7d9
Pierce <ppuckette@gmail.com>
2022-06-27 20:30:15 -0400

    tab fetch jobs

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,10 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+//
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  return <h2>tabs project setup</h2>
+	const [loading, setLoading] = useState(true);
+	const [jobs, setJobs] = useState([]);
+	const [value, setValue] = useState(0);
+	//setup function to fetch jobs from api
+	const fetchJobs = async () => {
+		const response = await fetch(url);
+		const newJobs = await response.json();
+		setJobs(newJobs);
+		setLoading(false);
+	};
+	//setup useEffect to call callback function fetchJobs once
+	useEffect(() => {
+		fetchJobs();
+	}, []);
+	//setup loading conditions
+	if (loading) {
+		return (
+			<section classname="section loading">
+				<h1>loading...</h1>
+			</section>
+		);
+	}
+
+	return <h2>jobs</h2>;
 }
 
-export default App
+export default App;
